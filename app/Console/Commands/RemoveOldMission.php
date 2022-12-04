0d36b5f40fd01e2bd5ec1e04a872ba12a9630115
hoanggiang1123 <hoanggiang1123@gmail.com>
2022-12-04 20:58:29 +0700

    dad

--- app/Console/Commands/RemoveOldMission.php
+++ app/Console/Commands/RemoveOldMission.php
@@ -36,9 +36,5 @@ class RemoveOldMission extends Command
 
         $formatted_date = $date->format('Y-m-d H:i:s');
 
-        Mission::where('updated_at', '>', $formatted_date)->get()->each(function($item) {
-            Keyword::where('id', $item->keyword_id)->increment('traffic_count');
-            $item->delete();
-        });
     }
 }
