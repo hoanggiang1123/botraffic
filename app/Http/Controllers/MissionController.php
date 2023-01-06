78d255fb334a1fe318ce37fdcbe7d8108f24c06d
DevG <hoanggiang1123@gmail.com>
2023-01-06 18:57:32 +0800

    aaaddf

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -444,7 +444,7 @@ class MissionController extends Controller
             ->where('traffic_count', '>', 0)
             ->when(is_array($notAllowDomains) && count($notAllowDomains) > 0, function($query) use($notAllowDomains) {
                 $query->whereNotIn('domain', $notAllowDomains);
-            })->orderBy('created_at', 'asc')
+            })->orderBy('created_at', 'desc')
             ->first();
 
 
