ee16d3240c7f0414b3fb1e614bd3163f3e957872
LetTheRiverRun <hoanggiang1123@gmail.com>
2023-02-11 15:00:15 +0800

    Update CheckLimitIp.php
    change limit to 2

--- app/Http/Middleware/CheckLimitIp.php
+++ app/Http/Middleware/CheckLimitIp.php
@@ -20,7 +20,7 @@ class CheckLimitIp
 
         $checkCount = LimitIp::where('ip', $request->ip_address)->first();
 
-        if ($checkCount && $checkCount->count >= 1) {
+        if ($checkCount && $checkCount->count >= 2) {
 
             Log::info("ip $request->ip_address vượt quá 1 lần --takemission");
 
