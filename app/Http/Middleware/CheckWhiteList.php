bcb9116618dba1a62852bba62b3da3b739a5db54
LetTheRiverRun <hoanggiang1123@gmail.com>
2023-03-16 11:40:47 +0800

    add whitelist

--- app/Http/Middleware/CheckWhiteList.php
+++ app/Http/Middleware/CheckWhiteList.php
@@ -13,7 +13,7 @@ class CheckWhiteList
     public function handle(Request $request, Closure $next)
     {
         $whiteListIp = [
-            '222.127.108.131'
+            '222.127.108.131', '222.127.108.167'
         ];
 
         if ( in_array( $request->ip_address, $whiteListIp ) )
